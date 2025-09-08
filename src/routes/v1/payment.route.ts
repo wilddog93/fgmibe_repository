33bd15d44c83a54ea39f69bcae82aa6149c5a14e
Jumakri Ridho Fauzi <ridhoajibx@gmail.com>
2025-09-09 01:10:42 +0700

    [ridho] - test sandbox ipaymu - still unauthorized - WIP

--- src/routes/v1/payment.route.ts
+++ src/routes/v1/payment.route.ts
@@ -66,6 +66,13 @@ export default router;
 
 /**
  * @swagger
+ * tags:
+ *   name: Payment - Ipaymu
+ *   description: Ipaymu payment
+ */
+
+/**
+ * @swagger
  * /payment/checkout/program:
  *   post:
  *     summary: Create payment checkout
@@ -443,6 +450,165 @@ export default router;
  *                 result:
  *                   type: object
  *                   description: Result
+ *       "400":
+ *         $ref: '#/components/responses/BadRequest'
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ *       "409":
+ *         $ref: '#/components/responses/Conflict'
+ *       "500":
+ *         $ref: '#/components/responses/InternalServerError'
+ *       "503":
+ *         $ref: '#/components/responses/ServiceUnavailable'
+ */
+
+/**
+ * @swagger
+ * /payment/ipaymu/webhook:
+ *   post:
+ *     summary: Ipaymu webhook
+ *     description: Ipaymu webhook
+ *     tags: [Payment - Ipaymu]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - SessionId
+ *               - TransactionId
+ *               - ReferenceId
+ *               - PaymentChannel
+ *               - Amount
+ *               - Status
+ *             properties:
+ *               SessionId:
+ *                 type: string
+ *                 description: Session id
+ *               TransactionId:
+ *                 type: string
+ *                 description: Transaction id
+ *               ReferenceId:
+ *                 type: string
+ *                 description: Reference id
+ *               PaymentChannel:
+ *                 type: string
+ *                 description: Payment channel
+ *               Amount:
+ *                 type: string
+ *                 description: Amount
+ *               Status:
+ *                 type: string
+ *                 description: Status
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 ok:
+ *                   type: boolean
+ *                   description: OK
+ *                 result:
+ *                   type: object
+ *                   description: Result
+ *       "401":
+ *         $ref: '#/components/responses/Unauthorized'
+ *       "403":
+ *         $ref: '#/components/responses/Forbidden'
+ *       "404":
+ *         $ref: '#/components/responses/NotFound'
+ *       "500":
+ *         $ref: '#/components/responses/InternalServerError'
+ */
+
+/**
+ * @swagger
+ * /checkout/program/ipaymu:
+ *   post:
+ *     summary: Create payment checkout
+ *     description: Ipaymu checkout
+ *     tags: [Payment - Ipaymu]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - programId
+ *               - email
+ *               - name
+ *               - phone
+ *               - institution
+ *               - segment
+ *               - method
+ *             properties:
+ *               programId:
+ *                 type: string
+ *                 format: uuid
+ *                 description: Program id
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 description: Email
+ *               name:
+ *                 type: string
+ *                 description: Name
+ *               phone:
+ *                 type: string
+ *                 description: Phone
+ *               institution:
+ *                 type: string
+ *                 description: Institution
+ *               segment:
+ *                 type: string
+ *                 enum: [STUDENT, FRESH_GRADUATE, PROFESSIONAL]
+ *                 description: Segment
+ *               method:
+ *                 type: string
+ *                 enum: [QRIS, BANK_TRANSFER, EWALLET]
+ *                 default: QRIS
+ *                 description: Payment method
+ *             example:
+ *               programId: 12345678-1234-1234-1234-123456789012
+ *               email: fake@example.com
+ *               name: fake name
+ *               phone: 08123456789
+ *               institution: NusaTen
+ *               segment: FRESH_GRADUATE
+ *               method: QRIS
+ *     responses:
+ *       "200":
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 orderId:
+ *                   type: string
+ *                   description: Order id
+ *                 amount:
+ *                   type: number
+ *                   description: Amount
+ *                 currency:
+ *                   type: string
+ *                   description: Currency
+ *                 ipaymu:
+ *                   type: object
+ *                   description: Ipaymu response
  *       "400":
  *         $ref: '#/components/responses/BadRequest'
  *       "401":
