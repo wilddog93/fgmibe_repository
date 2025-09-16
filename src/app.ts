348c0046e150ae6d00bbd6463ff6bcceb2a2374e
Jumakri Ridho Fauzi <ridhoajibx@gmail.com>
2025-09-16 20:58:57 +0700

    [ridho] - change ecosystem - WIP

--- src/app.ts
+++ src/app.ts
@@ -68,7 +68,7 @@ passport.use('github', githubStrategy);
 
 // Routes
 app.get('/', (req, res) => {
-  res.json({ message: '🚀 Hello from FGMI API Boilerplate Development!' });
+  res.json({ message: '🚀 Hello from FGMI API Boilerplate Production!' });
 });
 
 // Health Check
